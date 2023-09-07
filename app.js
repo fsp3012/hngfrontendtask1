232acbabcc71708b0b8a98acbb32ec7819077e32
fsp3012 <fakoredeabdulafeezolamide@gmail.com>
2023-09-07 14:25:24 +0100

    updated elapsed time

--- app.js
+++ app.js
@@ -1,38 +1,14 @@
-// Function to update the current day of the week
-function updateCurrentDay() {
-  const daysOfWeek = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  const currentDate = new Date();
-  const dayOfWeek = daysOfWeek[currentDate.getDay()];
-  const currentDayElement = document.querySelector(
-    '[data-testid="currentDayOfTheWeek"]'
-  );
-  currentDayElement.textContent = dayOfWeek;
-}
-
-// Function to update the current UTC time
 function updateCurrentTime() {
-  const currentDate = new Date();
-  const utcTime = currentDate.toISOString().slice(20);
+  const startTime = new Date().getTime();
   const currentUTCTimeElement = document.querySelector(
     '[data-testid="currentUTCTime"]'
   );
-  currentUTCTimeElement.textContent = utcTime + " UTC";
+
+  function updateElapsedTime() {
+    const currentTime = new Date().getTime();
+    const elapsedTime = currentTime - startTime;
+    currentUTCTimeElement.textContent = elapsedTime;
+  }
+  setInterval(updateElapsedTime, 1000);
 }
-
-// Call the update functions initially
-updateCurrentDay();
 updateCurrentTime();
-
-// Update the current day and time every second
-setInterval(() => {
-  updateCurrentDay();
-  updateCurrentTime();
-}, 1000);
